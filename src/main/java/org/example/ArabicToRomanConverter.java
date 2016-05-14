a410489afc930c23009186c98014af4db109dcfd
student <student@lab>
2016-05-14 13:17:23 +0200

    komit

--- src/main/java/org/example/ArabicToRomanConverter.java
+++ src/main/java/org/example/ArabicToRomanConverter.java
@@ -3,7 +3,15 @@ package org.example;
 public class ArabicToRomanConverter {
 
 	public String convert(int arabic) {
- if(arabic==1)
+		if (arabic == 1) 
+			return "I";
+		 else if (arabic == 5)
+			return "V";
+		else if (arabic == 10)
+			return "X";
+		else
+			return null;
+
 	}
 
 }
