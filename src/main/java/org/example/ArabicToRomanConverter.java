7b63da4fe5d79913f3273f856fdc7f20da6ef4ea
MojeImie <wiki.hsjh@op.pl>
2016-05-14 13:45:51 +0200

    squash: poprawiona klasa ArabicToRomanConverter i druga poprawka klasy konwertujacej liczby arabskie na rzymskie

--- src/main/java/org/example/ArabicToRomanConverter.java
+++ src/main/java/org/example/ArabicToRomanConverter.java
@@ -1,20 +1,25 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArabicToRomanConverter {
+
+	Map<Integer, String> mapping = new HashMap<>();
+
+	ArabicToRomanConverter() {
+		uzupelnijMapping();
+	}
+
+	private void uzupelnijMapping() {
+		mapping.put(1, "I");
+		mapping.put(5, "V");
+		mapping.put(10, "X");
+	}
 
 	public String convert(int arabic) {
 
-		if (arabic == 1) {
-			return "I";
-		}
-		if (arabic == 5) {
-			return "V";
-		}
-		if (arabic == 10) {
-			return "X";
-		} else {
-			return null;
-		}
+		return mapping.get(arabic);
 	}
 
 }
