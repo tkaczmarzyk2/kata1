4f68d828f73d3ec9531dd4dd377c6c9cc97aefe8
Patryk <grabol1990@gmail.com>
2016-05-14 13:28:51 +0200

    impl`

--- src/main/java/org/example/ArabicToRomanConverter.java
+++ src/main/java/org/example/ArabicToRomanConverter.java
@@ -3,7 +3,12 @@ package org.example;
 public class ArabicToRomanConverter {
 
 	public String convert(int arabic) {
-		
+		if (arabic == 5) {
+			return "V";
+		}
+		if (arabic == 10) {
+			return "X";
+		}
 		return "I";
 	}
 
