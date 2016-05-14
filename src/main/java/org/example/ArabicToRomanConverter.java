474ebf6163ac336fae3039067189c7af99fbb391
Coffee13 <mateusz.warzyc@wp.pl>
2016-05-14 13:05:32 +0200

    first 1 , 5, 10 arabic to roman implementation

--- src/main/java/org/example/ArabicToRomanConverter.java
+++ src/main/java/org/example/ArabicToRomanConverter.java
@@ -3,8 +3,14 @@ package org.example;
 public class ArabicToRomanConverter {
 
 	public String convert(int arabic) {
-		
-		return "I";
+		if (arabic == 1) {
+			return "I";
+		} else if (arabic == 5) {
+			return "V";
+		} else if (arabic == 10) {
+			return "X";
+		}
+		return "ta liczba nie jest obsługiwana";
 	}
 
 }
