7d30f0c0b882e1b4e09870099e3fdd753bf1a0b3
sdibfsdf <noadfnoadnfo>
2016-05-14 13:42:49 +0200

    commitZHashMap

--- src/main/java/org/example/ArabicToRomanConverter.java
+++ src/main/java/org/example/ArabicToRomanConverter.java
@@ -1,17 +1,26 @@
 package org.example;
+import java.util.Map;
+import java.util.HashMap;
 
 public class ArabicToRomanConverter {
 
+	private Map<Integer,String> mapping=new HashMap<>();
+	
 	public String convert(int arabic) {
 		
-		if(arabic==1)
+		/*if(arabic==1)
 			return "I";	
 		if(arabic==5)
 			return "V";
 		if(arabic==10)
-			return "X";
+			return "X";*/
+		mapping.put(1, "I");
+		mapping.put(5, "V");
+		mapping.put(10, "X");
 		
-		return null;
+		
+		
+		return mapping.get(arabic);
 		
 	}
 
