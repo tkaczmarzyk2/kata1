cd8d3dacfa6e3b1d6a1786b5ed1f2359ad4b5642
MojeImie <wiki.hsjh@op.pl>
2016-05-14 13:14:34 +0200

    drugi Test

--- src/main/java/org/example/ArabicToRomanConverter.java
+++ src/main/java/org/example/ArabicToRomanConverter.java
@@ -3,8 +3,18 @@ package org.example;
 public class ArabicToRomanConverter {
 
 	public String convert(int arabic) {
-	
-		return "I";
+
+		if (arabic == 1) {
+			return "I";
+		}
+		if (arabic == 5) {
+			return "V";
+		}
+		if (arabic == 10) {
+			return "X";
+		} else {
+			return null;
+		}
 	}
 
 }
