8b04140d2f2110433ca71e46ce1003d6407fb969
adej <adejpejej@interia.eu>
2016-05-14 12:11:54 +0200

    testowy commit

--- src/main/java/org/example/ArabicToRomanConverter.java
+++ src/main/java/org/example/ArabicToRomanConverter.java
@@ -1,8 +1,13 @@
 package org.example;
 
-public class ArabicToRomanConverter {
 
+public class ArabicToRomanConverter {
+	
+	
+
+	
 	public String convert(int arabic) {
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
