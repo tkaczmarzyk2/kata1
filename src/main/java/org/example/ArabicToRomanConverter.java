bdd4ae9df2df9203fa360df640c5b3bbb42d5c00
Roland Freier <Roland Freier>
2016-05-14 13:05:40 +0200

    commit dla kolejnych testów

--- src/main/java/org/example/ArabicToRomanConverter.java
+++ src/main/java/org/example/ArabicToRomanConverter.java
@@ -3,8 +3,13 @@ package org.example;
 public class ArabicToRomanConverter {
 
 	public String convert(int arabic) {
-		
-		return "I";
+		if (arabic == 1) {
+			return "I";
+		} else if (arabic == 5) {
+			return "V";
+		} else {
+			return "X";
+		}
 	}
 
 }
