c12060903ec4d2a100ddf4607a725428fc24b9d3
Patryk <grabol1990@gmail.com>
2016-05-14 13:28:51 +0200

    impl` i dodano hashmap

--- src/main/java/org/example/ArabicToRomanConverter.java
+++ src/main/java/org/example/ArabicToRomanConverter.java
@@ -1,15 +1,27 @@
 package org.example;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class ArabicToRomanConverter {
 
-	public String convert(int arabic) {
-		if (arabic == 5) {
-			return "V";
-		}
-		if (arabic == 10) {
-			return "X";
-		}
-		return "I";
+	private Map<Integer, String> mapping = new HashMap<>();
+
+	public ArabicToRomanConverter() {
+		super();
+
+		mapping.put(1, "I");
+
+		mapping.put(5, "V");
+
+		mapping.put(10, "X");
+
+	}
+	
+	public String convert(int arabic){
+		
+		
+		return mapping.get(arabic);
 	}
 
 }
